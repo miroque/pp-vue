b1b4fb6bffeae65395e8976f753e7ebdbd941fa1
Panov Aleksander <a.panov@sysolutions.ru>
2019-12-16 17:26:25 +0700

    :star: initital commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var avatar = new Vue({
+    el: '#avatar',
+    data: {
+      message: 'HERE Should be an avatar picture'
+    }
+  })
+
+  var fio = new Vue({
+    el: '#fio',
+    data: {
+      message: 'Aleksander Panov'
+    }
+  })
+
+  var level = new Vue({
+    el: '#level',
+    data: {
+      message: '35'
+    }
+  })
+
+  var seeds = new Vue({
+    el: '#seeds',
+    data: {
+      message: 'programmer, artists'
+    }
+  })
